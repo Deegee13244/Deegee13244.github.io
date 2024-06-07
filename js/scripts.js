ba5c4836161be1597bcddd006573c9bd466f7fa1
Brad Didier <braddidier13244@gmail.com>
2024-06-07 14:00:34 -0400

    Moving js out of html

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,80 @@
 // Scripts
 // 
 
+//API Integration - Grabs Joke from Jokeapi
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = "YOUR_API_KEY";
+
+    function fetchJoke() {
+        const apiUrl = "https://v2.jokeapi.dev/joke/Any";
+
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    console.error("Error fetching joke", data.message);
+                    document.getElementById("joke").innerText = "Error occurred while fetching a joke.";
+                } else if (
+                    data.flags.nsfw || data.flags.racist || data.flags.sexist || data.flags.explicit
+                ) {
+                    console.error("Joke not allowed", data.message);
+                    document.getElementById("joke").innerText = "Joke contained innapropriate content. Refresh or wait for a different joke.";
+                } else {
+                    const joke = data.joke || `${data.setup} ${data.delivery}`;
+                    document.getElementById("joke").innerText = joke;
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching joke", data.message);
+                document.getElementById("joke").innerText = "Error occurred while fetching a joke.";
+            });
+    }
+
+    fetchJoke();
+
+    setInterval(fetchJoke, 60000);
+});
+
+// Function to set or retrieve the value of a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0)
+          return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function displayWelcomeMessage() {
+    var lastVisit = getCookie("lastVisit");
+    if (!lastVisit) {
+        setCookie("lastVisit", new Date().toISOString(), 30); // Set cookie to expire in 30 days
+        alert("Welcome to my homepage for the first time!");
+      } else {
+        // Returning visit
+        var lastVisitDate = new Date(lastVisit);
+        alert(
+          "Welcome back! Your last visit was " +
+            lastVisitDate.toLocaleString()
+        );
+        setCookie("lastVisit", new Date().toISOString(), 30);
+    }
+}
+window.onload = displayWelcomeMessage;
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
